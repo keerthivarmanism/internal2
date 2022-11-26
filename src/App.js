21e0a001bff786cbc5f64c6fe7f0aa0fcdb0627a
keerthivarmanism <119048367+keerthivarmanism@users.noreply.github.com>
2022-11-26 16:28:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Dropdown from './components/Dropdown';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <Dropdown/>
+    </div>
+  );
+}
+
+export default App;
